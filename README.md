b36447d5ef3f8280e8687947d78288f980773954
LarsKarlsen82 <lpuk82@gmail.com>
2024-02-16 11:16:55 +0100

    update

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+Bruger oprettet som test: test@techcollege.dk med kode: password
+
+Fikset: 
+* Antal varer kan ikke gå i negativt i PosterDetails
+* I PosterList fjernet synligt scrollbar med     
+
+    ::-webkit-scrollbar {
+        display:none;
+      }
+ I Global.style.js
+ * I CartList sat den til at slette en item/vare af gangen og når man holder affaldikonet i 3 sekunder, slettes ordrerækken helt
--- src/Components/CartList/CartList.style.js
+++ src/Components/CartList/CartList.style.js
@@ -1,25 +1,43 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const CartListContainer = styled.div`
-	&>div {
-		&:first-child, &:last-child {
-			font-weight: bold;
-            font-size: 1.2rem;
-		}
+  & > div {
+    &:first-child,
+    &:last-child {
+      font-weight: bold;
+      font-size: 1.2rem;
+    }
 
-		display: grid;
-		grid-template-columns: 2fr 1fr 1fr 1fr;
-		grid-row-gap: 0;
-		padding: 0.5rem 0;
-		border-bottom: solid 1px #ccc;
+    display: grid;
+    grid-template-columns: 2fr 1fr 1fr 1fr;
+    grid-row-gap: 0;
+    padding: 0.5rem 0;
+    border-bottom: solid 1px #ccc;
 
-		div {
-			text-align: right;
+    div {
+      text-align: right;
 
-			&:first-child {
-				text-align: left;
-			}
-		}
-	}
-`
+      &:first-child {
+        text-align: left;
+      }
+    }
+  }
 
+  @media (max-width: 768px) {
+    & > div {
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-row-gap: 0;
+      padding: 0.5rem 0;
+      border-bottom: solid 1px #ccc;
+
+      div {
+        text-align: left;
+
+        &:first-child {
+          text-align: left;
+        }
+      }
+    }
+  }
+`;
